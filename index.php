bc335f75481fa6d64f0a072e4cecf7eb44c2ade8
Shireen Jain <100492457+shireenjain@users.noreply.github.com>
2023-08-12 10:32:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Tracker</title>
+        <title>Task-Spotlight</title>
         <link rel="icon" href="images/favicon.ico" type="image/x-icon"/>
         <link rel="stylesheet" type="text/css" href="./css/normalize.css">
         <link rel="stylesheet" type="text/css" href="./css/master.css">
@@ -34,15 +34,15 @@
                     <table>
                         <tr>
                             <td width: "60%" valign:"top">
-                                <h2>Join Tracker Today!</h2> <br>
-                                <p> Whether it’s for work, a side project or even the next family vacation, Tracker helps your team stay organized.</p>
+                                <h2>Join TaskSpotlight today!</h2> <br>
+                                <p>Can't remember and manage your tasks?! We are here to notify you!</p>
                             </td>
                             <td width:"40%" valign:"top">
                                 <form action="#" method="post">
                                     <input type="text" name="fname" size="25" placeholder="First Name" required> <br>
                                     <input type="text" name="lname" size="25" placeholder="Last Name" required> <br>
                                     <input type="text" name="username" size="25" placeholder="Username" required> <br>
-                                    <input type="text" name="email" size="25" placeholder="Email Adress" required> <br>
+                                    <input type="text" name="email" size="25" placeholder="Email Address" required> <br>
                                     <input type="password" name="password" size="25" placeholder="Password" required> <br>
                                     <input type="password" name="passwordcheck" size="25" placeholder="Repeat Password" required> <br>
                                     <input type="submit" name="register" value="Sign Up">
@@ -59,7 +59,5 @@
                 <p>2017 &copy; Tracker</p>
             </div>
         </div>
-
-
     </body>
 </html>
