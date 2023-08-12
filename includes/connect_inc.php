1ccc7409ae525abcde6a211c7cd298d80bf1e08f
Shireen Jain <100492457+shireenjain@users.noreply.github.com>
2023-08-12 11:53:19 +0530

    Edits in PHP and SQL files

--- includes/connect_inc.php
+++ includes/connect_inc.php
@@ -2,7 +2,7 @@
     $dbhost='localhost';
     $dbuser='root';
     $dbpassword='';
-    $database='DB_TRACKER';
+    $database='project_db';
     $connect = mysqli_connect($dbhost, $dbuser, $dbpassword) or die("Connection Error: " .mysqli_error());
     mysqli_select_db($connect, $database) or die("Error conecting to database.");
  ?>
